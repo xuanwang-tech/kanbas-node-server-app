cb9b5e95ee90824146c0674bdd89fc928ddc12ae
xuanwang <wang.xuan5@northeastern.edu>
2023-12-01 14:42:35 -0600

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import UserRoutes from "./users/routes.js";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignment/routes.js";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 const app = express();
 // app.use(
 //   cors({
@@ -14,6 +19,7 @@ const app = express();
 //   }));
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app)
