6541c6b25151fdd885a5e2e60970ee2732992ca9
xuanwang <wang.xuan5@northeastern.edu>
2023-12-04 16:39:38 -0600

    a6

--- app.js
+++ app.js
@@ -8,16 +8,54 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignment/routes.js";
 import mongoose from "mongoose";
+import session from "express-session";
+const CONNECTION_STRING = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
+const db = mongoose.connection;
+db.on('connected', () => {
+  console.log('Connected to MongoDB');
+  console.log(`Database name: ${db.name}`);
+});
 const app = express();
 // app.use(
 //   cors({
 //     credentials: true,
 //     origin: process.env.NODE_ENV === "production" ? process.env.FRONTEND_URL : process.env.FRONTEND_URL_LOCAL,
 //   }));
-app.use(cors());
+
+
+// app.use(cors());
+app.use(cors({
+    credentials: true,
+    // origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL
+  })
+);
+// const sessionOptions = {
+//     secret: "any string",
+//     resave: false,
+//     saveUninitialized: false,
+//   };
+//   app.use(
+//     session(sessionOptions)
+//   );
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 app.use(express.json());
 UserRoutes(app);
 AssignmentRoutes(app);
--- users/dao.js
+++ users/dao.js
@@ -6,8 +6,15 @@ export const findUserByUsername = (username) =>
   model.findOne({ username: username });
 export const findUserByCredentials = (username, password) =>
   model.findOne({ username, password });
+// export const findAllUsers = () => model.find();
+export const findUsersByRole = (role) => model.find({ role: role });
 export const updateUser = (userId, user) =>
   model.updateOne({ _id: userId }, { $set: user });
+
+
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
 
 // export const findUserByCredentials = (usr, pass) => model.findOne({ username: usr, password: pass });
+
+// export const findUserByUsername = (username) =>
+//   model.findOne({ username: username });
